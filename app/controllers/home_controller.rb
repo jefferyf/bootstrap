e871f5db84266a9b0841f595b0e73565b9f73c9f
Jeff Miller <jeffery.f@gmail.com>
2012-02-25 15:44:48 -0600

    files

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  
+  def index
+    #flash[:error] = "Invalid email or password"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boostrap::Application.config.secret_token = '03b0236eee0f41d60b0a7e724543ce0b623127efe676d0d2f9da833a8f5b44df00eb5053570d62c0b188f567bcd656c28cb3525de88fc1a9cb08aad431142c50'
